a319d42772387e502b37d5d1290afd9f953278f8
sohseyedi <sohseyedi@gmail.com>
2022-01-24 15:17:55 +0330

    check in cart

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Products.scss'
 import { ProductsData } from './../../Data/data';
-import { useCartDispatch } from './../../Context/CartContext';
+import { useCart, useCartDispatch } from './../../Context/CartContext';
 
 const Products = () => {
 
     const products = ProductsData;
+    const { cartItems } = useCart();
     const dispatch = useCartDispatch();
 
     const addItemHandler = (item) => {
         dispatch({ type: 'ADD_ITEM', payload: item });
+    }
+
+    const checkInCart = (cartItems, item) => {
+        return cartItems.find(c => c.id === item.id)
     }
 
     return (
@@ -24,7 +29,9 @@ const Products = () => {
                                     <div className="products-content__box-body__name">{item.name}</div>
                                     <div className="products-content__box-body__price">{item.price}/000</div>
                                 </div>
-                                <div className="products-content__box-footer" onClick={() => addItemHandler(item)}>خرید محصول</div>
+                                <div className={checkInCart(cartItems, item) ? "products-content__box-footer active-btn" : "products-content__box-footer"} onClick={() => addItemHandler(item)}>
+                                    {checkInCart(cartItems, item) ? 'ثبت سفارش' : ' خرید محصول'}
+                                </div>
                             </div>
                         ))
                     }
